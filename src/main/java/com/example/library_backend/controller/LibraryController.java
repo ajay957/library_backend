7de47f849e4ef8960df631b423b518017d8f6a6b
Ajay957 <ajay74.aa82@gmail.com>
2022-12-06 03:35:24 +0530

    first commit

--- src/main/java/com/example/library_backend/controller/LibraryController.java
+++ src/main/java/com/example/library_backend/controller/LibraryController.java
@@ -1,4 +1,12 @@
 package com.example.library_backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LibraryController {
+    @GetMapping("/")
+    public String HomePage(){
+        return "welcome to library home page";
+    }
 }
